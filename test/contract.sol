060ef6350d37a892c0ab62ff424b4ea15bf53b2b
Juan Barba Mateo <jbarba@clevertask.com>
2020-12-03 00:55:33 +0100

    Compile

--- test/contract.sol
+++ test/contract.sol
@@ -1,5 +1,7 @@
+// SPDX-License-Identifier: None
+pragma solidity ^0.7.5; 
 contract HelloWorld{
-    function myfunc(address _myAddress) {
+    function myfunc(address _myAddress) public {
         //Do stuff
     }
 }
